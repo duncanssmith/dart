be814d02c04cd4df96028df10beaf26a8b3bdc6f
Duncan Smith <duncansmith@Duncans-Air.cust.communityfibre.co.uk>
2024-02-17 18:39:06 +0000

    Remove incorrect code in group pages view

--- app/views/pages/group.blade.php
+++ app/views/pages/group.blade.php
@@ -10,19 +10,8 @@
 
 @section('content')
 
-
-
-                @if (false)
-
-                <div data-slick='{"slidesToShow": {{$columns}}, "slidesToScroll": 1}'>
-                    @foreach ($works as $work)
-                      <div><h3><img src="/media/images/320/{{ $work->reference }}.jpg"></h3></div>
-                    @endforeach
-                </div>
-
-                @elseif (true)
-
     <div class="container">
+
         <div class="row">
             <div class="col-md-6">
 
@@ -30,13 +19,12 @@
                     <h4>{{ $group->name }}</h4>
                     @if(!$group->display)
                         @include('partials.hidden')
-                    @endi
+                    @endif
                 </div>
 
 
-
                     <table>
-                    @foreach($works as $work)
+                    @foreach($works as $i => $work)
 
                         @if ($i % $columns == 0)
                             @if ($i==0)
@@ -48,12 +36,7 @@
                         @endif
                             <td class="work-container">
     	    	                <a id="modal-{{ $work->reference }}" data-toggle="modal" data-target="#item-show-{{ $work->reference }}" ><img src="/media/images/320/{{ $work->reference }}.jpg"></a><br>
-
-                                <!--<p>{{ $work->title }} </p>-->
-                                <!-- {{ $work->media }} <br>
-                                     {{ $work->dimensions }} <br>
-                                     {{ $work->work_date }} -->
-                                <!-- Button trigger modal -->
+                                <!-- Modal trigger button -->
                                 <br>
                                 <a id="modal-{{ $work->reference }}" href="/pagework/{{$work->id}}?group={{ $group->id}}" class="btn btn-xs btn-default"><i class="fa fa-arrow-right" style="color:#999;"></i> {{ $work->title }}</a>
                                 <!-- Image display modal ////////////////////////////////////////////////////////// -->
@@ -105,50 +88,4 @@
         </div>
     </div> <?php // end container?>
 
-            @else
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-
-                  <!-- Indicators -->
-                  <ol class="carousel-indicators">
-
-                    <?php $i = 0 ?>
-                    <?php $class = 'active'?>
-
-                    @foreach ($works as $work)
-                      <li data-target="#carousel-example-generic" data-slide-to="{{$i}}" class="<?php echo $i==0 ? $class : ''?>"></li>
-                      <?php $i++ ?>
-                    @endforeach
-
-                  </ol>
-
-                  <!-- Wrapper for slides -->
-
-                  <div class="carousel-inner">
-                  <?php $i = 0 ?>
-                  @foreach ($works as $work)
-
-
-                    <div class="item <?php echo $i==0 ? $class : ''?>">
-                      <img src="/media/images/640/{{ $work->reference }}.jpg">
-                      <div class="carousel-caption">
-                          <h3>Caption Text {{ $i }}</h3>
-                      </div>
-                    </div>
-
-
-                  <?php $i++ ?>
-
-                  @endforeach
-
-                  </div>
-                  <!-- Controls -->
-                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                  </a>
-                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                  </a>
-                </div> <!-- Carousel -->
-
-            @endif
-@stop
+@stop    
